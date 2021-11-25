7f7f9919c462689885d064f356e7e51654a0722b
Abdul Muizz Abdul Jalil <abmuizz@gmail.com>
2021-11-25 04:23:55 +0100

    change ensemble model again

--- yolov5/train.py
+++ yolov5/train.py
@@ -126,14 +126,7 @@ def train(hyp,  # path/to/hyp.yaml or hyp dictionary
     else:
         model = Model(cfg, ch=4, nc=nc, anchors=hyp.get('anchors')).to(device)  # create
 
-    #MyModel
-    sensor_fusion_model = ConvAutoencoder()
-    features = nn.ModuleList(sensor_fusion_model.children())[:-1]
-    model1 = nn.Sequential(*features)
-    features2 = nn.ModuleList(model.children())[:-1]
-    model2 = nn.Sequential(*features)
-    model = nn.Sequential(model1, model2)
-    #model = MyModel(sensor_fusion_model, model)
+    
 
     # Freeze
     freeze = [f'model.{x}.' for x in range(freeze)]  # layers to freeze
@@ -270,6 +263,16 @@ def train(hyp,  # path/to/hyp.yaml or hyp dictionary
     model.hyp = hyp  # attach hyperparameters to model
     model.class_weights = labels_to_class_weights(dataset.labels, nc).to(device) * nc  # attach class weights
     model.names = names
+
+    #MyModel
+    sensor_fusion_model = ConvAutoencoder()
+    sensor_fusion_model.load_state_dict(csd, strict=False)
+    features = nn.ModuleList(sensor_fusion_model.children())[:-1]
+    model1 = nn.Sequential(*features)
+    features2 = nn.ModuleList(model.children())[:-1]
+    model2 = nn.Sequential(*features)
+    model = nn.Sequential(model1, model2)
+    #model = MyModel(sensor_fusion_model, model)
 
     # Start training
     t0 = time.time()
